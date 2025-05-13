b0aa3a429253b22007ac1aad3e656088a0b54fac
Guillaume Letilly <gletilly@c2r1p4.42lehavre.fr>
2025-05-13 22:26:01 +0200

    add struct s_token

--- parsing/parsing.h
+++ parsing/parsing.h
@@ -3,6 +3,12 @@
 
 # include "../minishell.h"
 
+typedef struct s_token
+{
+	char			*value;
+	struct s_token	*next;
+}	t_token;
+
 t_shell		*p_init_lst(void);
 void		p_create_node_lst(t_shell **shell);
 void		p_free_all_lst(t_shell **head);
