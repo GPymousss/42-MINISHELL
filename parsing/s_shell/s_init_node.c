d49f30439b4997cb2b2108ceb0fdf7c94fa3395b
Guillaume Letilly <gletilly@c2r1p4.42lehavre.fr>
2025-05-16 03:33:09 +0200

    STDIN_FILENO / STDOUT_FILENO s_shell struct

--- parsing/s_shell/s_init_node.c
+++ parsing/s_shell/s_init_node.c
@@ -11,8 +11,8 @@ t_shell	*p_init_lst(void)
 	lst->cmd = NULL;
 	lst->redir = NULL;
 	lst->nb_pipe = 0;
-	lst->fd_in = 0;
-	lst->fd_out = 0;
+	lst->fd_in = STDIN_FILENO;
+	lst->fd_out = STDOUT_FILENO;
 	lst->hdc_idx = -1;
 	lst->wstatus = 0;
 	return (lst);
