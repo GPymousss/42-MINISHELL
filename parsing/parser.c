5774fe7dc9f34b9964cc555d5f32cd9c57012f73
LeoLanganay <leo.langanay@gmail.com>
2025-07-08 10:40:32 +0200

    fix exit status syntax

--- parsing/parser.c
+++ parsing/parser.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gletilly <pymousss.dev@gmail.com>          +#+  +:+       +#+        */
+/*   By: llangana <llangana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 14:28:19 by llangana          #+#    #+#             */
-/*   Updated: 2025/07/04 23:14:10 by gletilly         ###   ########.fr       */
+/*   Updated: 2025/07/08 10:40:02 by llangana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-static int	syntax_error(const char *msg)
+static int	syntax_error(t_shell *shell, const char *msg)
 {
 	ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
 	if (msg)
@@ -20,10 +20,11 @@ static int	syntax_error(const char *msg)
 	else
 		ft_putstr_fd("newline", 2);
 	ft_putstr_fd("'\n", 2);
+	shell->exit_status = 2;
 	return (-1);
 }
 
-static int	handle_redirection(t_cmd *cmd, t_token **cur)
+static int	handle_redirection(t_shell *shell, t_cmd *cmd, t_token **cur)
 {
 	t_symbol	symbol;
 	const char	*unexpected;
@@ -32,21 +33,21 @@ static int	handle_redirection(t_cmd *cmd, t_token **cur)
 	symbol = get_redir_symbol((*cur)->value);
 	next = (*cur)->next;
 	if (!next)
-		return (syntax_error(NULL));
+		return (syntax_error(shell, NULL));
 	if (next->type != TOKEN_WORD)
 	{
 		unexpected = next->value;
-		return (syntax_error(unexpected));
+		return (syntax_error(shell, unexpected));
 	}
 	add_redir_to_cmd(cmd, symbol, next->value);
 	*cur = next->next;
 	return (0);
 }
 
-static int	handle_pipe(t_cmd **cmd, t_token **cur)
+static int	handle_pipe(t_shell *shell, t_cmd **cmd, t_token **cur)
 {
 	if (!(*cmd)->args)
-		return (syntax_error((*cur)->value));
+		return (syntax_error(shell, (*cur)->value));
 	(*cmd)->next = new_cmd();
 	if (!(*cmd)->next)
 		return (-1);
@@ -67,12 +68,12 @@ int	parse_tokens_to_cmds(t_shell *shell, t_token *tokens)
 	{
 		if (cur->type == TOKEN_PIPE)
 		{
-			if (handle_pipe(&cmd, &cur) != 0)
+			if (handle_pipe(shell, &cmd, &cur) != 0)
 				return (-1);
 		}
 		else if (cur->type >= TOKEN_REDIR_IN && cur->type <= TOKEN_APPEND)
 		{
-			if (handle_redirection(cmd, &cur) != 0)
+			if (handle_redirection(shell, cmd, &cur) != 0)
 				return (-1);
 		}
 		else
